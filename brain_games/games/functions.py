2d4e062b9d2366d16b8e8bfa0a8d6a50fc30d28a
Jukka <tor.of.minoc@gmail.com>
2022-05-07 14:40:57 +0300

    deleting debagging "print" line

--- brain_games/games/functions.py
+++ brain_games/games/functions.py
@@ -7,7 +7,6 @@ def ask_and_check(rules, question_and_answer):
     print(f'Hello, {user_name}!\n{rules}')
     for tries in range(3):  # 3 attempts at default
         question, correct_answer = question_and_answer()
-        print(correct_answer)
         print(f'Question: {question}')
         players_answer = prompt.string('Your answer: ')
         if players_answer == correct_answer:
