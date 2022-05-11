5b5182db89cc443c75b720fa2fa8eccdbe7a24e2
Jukka <tor.of.minoc@gmail.com>
2022-05-11 21:05:57 +0300

    fixing prime.py to generate prime numbers sequence only once when game starts

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,18 +3,16 @@ import random
 
 
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+primes_limit = 101  # in range 0, 100
 
-
-def game():
-    random_number = random.randrange(2, 100)
 
 # Sieve of Eratosthenes to generate prime numbers sequence
-    primes_limit = 101  # in range 0, 100
+def erathosthenes_sieve(primes_limit):
 
-# prime list initialization
+    # prime list initialization
     prime_list = []
 
-# boolean array for primes_limit range
+    # boolean array for primes_limit range
     boolean_array = [True] * primes_limit
 
     for i in range(2, primes_limit):
@@ -24,6 +22,14 @@ def game():
     for i in range(2, primes_limit):
         if boolean_array[i]:
             prime_list.append(i)
+    return prime_list
+
+
+prime_list = erathosthenes_sieve(primes_limit)
+
+
+def game():
+    random_number = random.randrange(2, 100)
     question = random_number
     correct_answer = 'yes' if random_number in prime_list else 'no'
     return [question, correct_answer]
